d4cdd0ed49e75f3dd25d0f9e5a07e55008e0fed8
Devesh <kadamdevesh969@gmail.com>
2026-08-21 11:08:45 +0530

    Main Changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://insight-flow-lemon-phi.vercel.app/"
+        "https://insight-flow-lemon-phi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
